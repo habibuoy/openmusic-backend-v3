a243e10344b271eba674995f3db69bb4633c6b27
habibuoy <habibiemuhammad90@gmail.com>
2025-07-12 23:28:00 +0700

    Chore: Update album handlers to use the convenience response method

--- src/api/albums/handlers.js
+++ src/api/albums/handlers.js
@@ -1,3 +1,5 @@
+const { created, succeed } = require('../responseObject');
+
 class AlbumHandler {
   constructor(albumService, albumValidator) {
     this._service = albumService;
@@ -11,14 +13,7 @@ class AlbumHandler {
 
     const result = await this._service.addAlbum({ name, year });
 
-    const response = h.response({
-      status: 'success',
-      data: {
-        albumId: result.id,
-      },
-    });
-    response.code(201);
-    return response;
+    return created(h, { data: { albumId: result.id } });
   }
 
   async getAlbumByIdHandler(request, h) {
@@ -26,12 +21,7 @@ class AlbumHandler {
 
     const result = await this._service.getAlbumById(id);
 
-    return h.response({
-      status: 'success',
-      data: {
-        album: result,
-      },
-    });
+    return succeed(h, { data: { album: result } });
   }
 
   async putAlbumByIdHandler(request, h) {
@@ -42,10 +32,7 @@ class AlbumHandler {
 
     const result = await this._service.updateAlbumById(id, { name, year });
 
-    return h.response({
-      status: 'success',
-      message: `Successfully updated album with id ${result.id}`,
-    });
+    return succeed(h, { message: `Successfully updated album with id ${result.id}` });
   }
 
   async deleteAlbumByIdHandler(request, h) {
@@ -53,10 +40,7 @@ class AlbumHandler {
 
     const result = await this._service.deleteAlbumById(id);
 
-    return h.response({
-      status: 'success',
-      message: `Succesfully deleted album with id ${result.id}`,
-    });
+    return succeed(h, { message: `Succesfully deleted album with id ${result.id}` });
   }
 }
 
