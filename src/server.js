a5a8e3b9b4ad4166b6e9094be0a15796c1173b32
habibuoy <habibiemuhammad90@gmail.com>
2025-07-12 16:19:23 +0700

    Chore: Add template files

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const Hapi = require('@hapi/hapi');
+require('dotenv').config();
+
+async function init() {
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+}
+
+init();
